986ba1a1689c19641a2adeebd3dda679ec494761
Peter SIkula <sikula@protonmail.com>
2019-06-06 14:41:56 -0400

    fix: fixing the share button URL not being copied to clipboard properly

--- src/pages/Admin/Home/components/IncomingFeed/FeedPanel.js
+++ src/pages/Admin/Home/components/IncomingFeed/FeedPanel.js
@@ -43,7 +43,7 @@ const ShareButton = class extends React.PureComponent {
         <textarea
           ref={textArea => (this.textArea = textArea)}
           readOnly
-          value={`https://saigar-ctf-demo.herokuapp.com/submission/${uuid}`}
+          value={`${window.location.protocol}//${window.location.hostname}/submission/${uuid}`}
           style={{ position: 'absolute', zIndex: '-1', height: 0, opacity: '0.01' }}
         />
       </React.Fragment>
