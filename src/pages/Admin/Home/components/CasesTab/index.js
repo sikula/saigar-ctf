eeaa16914c6eb79757f7cc4ab3932a196d0edbd6
Peter SIkula <sikula@protonmail.com>
2019-07-05 14:45:15 -0400

    feat: implements features to allow admins/judges to reject submissions with a reason and also allow admins/judges to view the processing history of a submission

---
+++ src/pages/Admin/Home/components/CasesTab/index.js
@@ -0,0 +1,107 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+
+import { differenceInDays } from 'date-fns'
+
+// Styles
+import {
+    Card, Button, Icon, H5, Tag 
+} from '@blueprintjs/core'
+import { IconNames } from '@blueprintjs/icons'
+
+// Custom Components
+import { SlidingPanelConsumer, SlidingPane } from '@shared/components/SlidingPane'
+import CaseInfoData from '@features/CaseInfo/components'
+
+const CASES_QUERY = gql`
+  query getCases {
+    event(order_by: { start_time: desc }, limit: 1) {
+      eventCasesByeventId {
+        case {
+          uuid
+          name
+          missing_since
+          missing_from
+        }
+      }
+    }
+  }
+`
+
+// TODO(peter): This entire file needs to be cleaned up and split up into its own components.
+// Its fine for now, but it is getting a little bit out of control.
+const CaseInfo = ({ isOpen, onRequestClose, ...otherProps }) => (
+  <SlidingPane
+    isOpen={isOpen}
+    onRequestClose={onRequestClose}
+    closeIcon={<Icon icon={IconNames.MENU_CLOSED} iconSize={20} />}
+  >
+    <SlidingPane.Header>
+      <SlidingPane.Header.Title title="Case Info" subtitle="Details on the case" />
+      <SlidingPane.Header.Actions onActionClick={onRequestClose}>
+        <a>Cancel</a>
+      </SlidingPane.Header.Actions>
+    </SlidingPane.Header>
+
+    <SlidingPane.Content>
+      <CaseInfoData caseID={otherProps.caseID} />
+    </SlidingPane.Content>
+  </SlidingPane>
+)
+
+const CaseInfoButton = ({ id }) => (
+  <SlidingPanelConsumer>
+    {({ openSlider }) => (
+      <Button
+        className="case-card__actions"
+        minimal
+        icon={<Icon icon={IconNames.INFO_SIGN} style={{ color: '#394B59' }} iconSize={20} />}
+        onClick={() => openSlider(CaseInfo, { caseID: id })}
+      />
+    )}
+  </SlidingPanelConsumer>
+)
+CaseInfoButton.propTypes = {
+  id: PropTypes.string.isRequired,
+}
+
+const CaseCard = ({ caseData }) => (
+  <div className="case-card__wrapper">
+    <Card id="case-card">
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start' }}>
+        <H5 className="case-card__header">{caseData.name}</H5>
+        <Tag round>{caseData.missing_from}</Tag>
+      </div>
+      <p>{`missing for: ${differenceInDays(new Date(), caseData.missing_since)} days`}</p>
+    </Card>
+    <div style={{ display: 'inline-flex', width: '100%', background: '#E1E8ED' }}>
+      <CaseInfoButton id={caseData.uuid} />
+    </div>
+  </div>
+)
+
+const CasesTab = () => (
+  <Query query={CASES_QUERY}>
+    {({ loading, error, data }) => {
+      if (loading) return null
+      if (error) return null
+
+      const cases = data.event[0].eventCasesByeventId
+
+      return (
+        <div className="case-card__grid">
+          <div className="case-card__row">
+            {cases.map(({ case: _case }) => (
+              <CaseCard key={_case.uuid} caseData={_case} />
+            ))}
+          </div>
+        </div>
+      )
+    }}
+  </Query>
+)
+
+
+export default CasesTab
---
+++ src/pages/Admin/Home/components/HistoryTab/SubmissionHistory.js
@@ -0,0 +1,152 @@
+/* eslint-disable react/require-default-props, react/forbid-prop-types, jsx-a11y/anchor-is-valid */
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Query, Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+
+import format from 'date-fns/format'
+
+// Styles
+import { Icon } from '@blueprintjs/core'
+import { IconNames } from '@blueprintjs/icons'
+
+import { Timeline, TimelineEvent } from 'react-event-timeline'
+
+// Custom Components
+import { SlidingPane, SlidingPanelConsumer } from '@shared/components/SlidingPane'
+import { AuthConsumer } from '@shared/components/AuthContext/context'
+
+
+const SUBMISSION_HISTORY = gql`
+  query submissionHistory($submissionID: uuid!) {
+    submission(where: {
+      uuid: { _eq: $submissionID }
+    }) {
+      submissionConfigurationByconfigId {
+        category
+        points
+      }
+      history(order_by: { processed_at: desc }) {
+        decision
+        processed_at
+        processedByUser {
+          role
+          username
+        }
+        rejected_reason
+      }
+    }
+  }
+`
+
+
+const SubmissionHistory = ({ isOpen, onRequestClose, ...otherProps }) => (
+  <SlidingPane
+    isOpen={isOpen}
+    onRequestClose={onRequestClose}
+    closeIcon={<Icon icon={IconNames.MENU_CLOSED} iconSize={20} />}
+  >
+    <SlidingPane.Header>
+      <SlidingPane.Header.Title title="Processing History" subtitle="Complete history of this submission" />
+      <SlidingPane.Header.Actions onActionClick={onRequestClose}>
+        <a>Cancel</a>
+      </SlidingPane.Header.Actions>
+    </SlidingPane.Header>
+
+    <SlidingPane.Content>
+      <SlidingPanelConsumer>
+        {({ closeSlider }) => (
+          <AuthConsumer>
+            {({ user }) => (
+              <Query query={SUBMISSION_HISTORY} variables={{ submissionID: otherProps.submissionID }} fetchPolicy="network-only" skip={!otherProps.submissionID}>
+                {({ data, loading, error }) => {
+                  if (!data) return null
+                  if (loading) return null
+                  if (error) return `Error: ${error}`
+
+                  const { submission  } = data
+                  const { history: submissionHistory, submissionConfigurationByconfigId: config } = submission[0]
+                  
+                  
+                  return (
+                    <div>
+                      <Timeline>
+                        {submissionHistory.map(submission => {
+                          let icon
+                          let color
+                          let title
+
+                          if (submission.decision === 'ACCEPTED') {
+                            icon = IconNames.TICK
+                            color = "#48AFF0"
+                            title = (
+                              <div>
+                                {submission.processedByUser.role.toLowerCase()}{' '}
+                                <strong>
+                                  ({submission.processedByUser.username})
+                                </strong>{' '}
+                                {submission.decision.toLowerCase()} the submission for +{config.points} points ({config.category.toLowerCase()})
+                              </div>
+                            )
+                          }
+
+                          if (submission.decision === 'REJECTED') {
+                            icon = IconNames.CROSS
+                            color = "#A82A2A"
+                            title = (
+                              <div>
+                                {submission.processedByUser.role.toLowerCase()}
+                                <strong>
+                                  ({submission.processedByUser.username})
+                                </strong>
+                                {submission.decision.toLowerCase()} the submission
+                              </div>
+                            )
+                          }
+
+                          if (submission.decision === 'STARRED') {
+                            icon = IconNames.STAR
+                            color = "#3DCC91"
+                            title = (
+                              <div>
+                                {submission.processedByUser.role.toLowerCase()}
+                                <strong>
+                                  ({submission.processedByUser.username})
+                                </strong>
+                                {submission.decision.toLowerCase()} the submission
+                              </div>
+                            )
+                          }
+
+                          return (
+                            <TimelineEvent
+                              title={title}
+                              createdAt={format(new Date(submission.processed_at), "HH:mm")} icon={<Icon icon={icon} />}
+                              contentStyle={{ background: '#EBF1F5' }}
+                              iconColor={"#FFF"}
+                              bubbleStyle={{ background: color }}
+                            >
+                              {submission.rejected_reason}
+                            </TimelineEvent>
+                          )
+                        })}
+                      </Timeline>
+                    </div>
+                  )
+                }}
+              </Query>
+            )}
+          </AuthConsumer>
+        )}
+      </SlidingPanelConsumer>
+    </SlidingPane.Content>
+  </SlidingPane>
+)
+
+SubmissionHistory.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onRequestClose: PropTypes.func.isRequired,
+  otherProps: PropTypes.any,
+}
+
+export default SubmissionHistory
