e0913d57fd4bd0dfebee053b3a86e8659b1233a1
test <test@test>
2019-11-24 22:13:41 +0000

    feat: working on the new registration process/team assignment

--- src/pages/Contestant/Challenges/index.js
+++ src/pages/Contestant/Challenges/index.js
@@ -27,14 +27,20 @@ const EVENT_START_TIME = gql`
   }
 `
 
-const CASE_LIST = gql`
-  query eventCases($auth0id: String!) {
+const USER_INFO = gql`
+  query userInfo($auth0id: String!) {
     user(where: { auth0id: { _eq: $auth0id } }) {
       acceptedTos
     }
     team(where: { user_team: { user: { auth0id: { _eq: $auth0id } } } }) {
       name
+      uuid
     }
+  }
+`
+
+const CASE_LIST = gql`
+  query eventCases($auth0id: String!) {
     event(order_by: { start_time: desc }, limit: 1) {
       eventCasesByeventId {
         case {
@@ -94,39 +100,13 @@ const CasesList = () => {
   if (error) return <div>{error.message}</div>
 
   const cases = data.event[0].eventCasesByeventId
-  const duser = data.user[0]
 
-  if(data.team.length === 0) {
-    return <TeamDialog />
-  }
-
-  const team = data.team[0]
 
   // @TODO(peter): Ensure that the event registration is done, then show the "event hasn't started message"
   return (
     <React.Fragment>
-      <nav className="challenges" style={{ background: '#fff' }}>
-        <div>
-          <ul>
-            <li>
-              <Link to="/rules">Rules</Link>
-            </li>
-            <li>
-              <Link to="/resources">Resources</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
       <div className="row">
         <div className="col-xs">
-          <div style={{ margin: 15 }}>
-            <H2>{team.name}</H2>
-          </div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-xs">
-          {duser.acceptedTos === false && <TosDialog />}
           <CaseGrid cases={cases} />
         </div>
       </div>
@@ -135,11 +115,18 @@ const CasesList = () => {
 }
 
 const ChallengesPage = () => {
+  const { user } = useContext(AuthContext)
+
   // GraphQL Layer
   const { error, loading, data } = useQuery(EVENT_START_TIME)
-
-  if (loading) return null
-  if (error) return <div>{error.message}</div>
+  const { error: userError, loading: userLoading, data: userData } = useQuery(USER_INFO, {
+    variables: {
+      auth0id: user.id
+    }
+  })
+  
+  if (loading || userLoading) return null
+  if (error || userError) return <div>{error.message}{userError.message}</div>
 
   if (!Array.isArray(data.event) || !data.event.length) {
     return <div>No Events Created yet</div>
@@ -147,30 +134,66 @@ const ChallengesPage = () => {
 
   const eventStarted = isWithinInterval(
     new Date(), // current Date
-    { start: new Date(data.event[0].start_time),
+    {
+      start: new Date(data.event[0].start_time),
       end: new Date(data.event[0].end_time)
     },
   )
 
-  if (!eventStarted) {
-    return (
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          height: '100%',
-        }}
-      >
-        <div style={{ maxWidth: '50%' }}>
-          <H2 style={{ textAlign: 'center' }}>Event Hasn't Started Yet</H2>
-          <H4>Refresh the page once the event has officially begun</H4>
-        </div>
-      </div>
-    )
+  if (userData.team.length === 0) {
+    return <TeamDialog />
   }
 
-  return <Can allowedRole="contestant" yes={() => <CasesList />} />
+  if (userData.user[0].acceptedTos === false) {
+    return <TosDialog />
+  }
+
+  const team = userData.team[0]
+
+  return (
+    <Can allowedRole="contestant" yes={() => (
+      <React.Fragment>
+        <nav className="challenges" style={{ background: '#fff' }}>
+          <div>
+            <ul>
+              <li>
+                <Link to="/rules">Rules</Link>
+              </li>
+              <li>
+                <Link to="/resources">Resources</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-xs">
+            <div style={{ margin: 15 }}>
+              <H2>{team.name} / CODE: {team.uuid}</H2>
+            </div>
+          </div>
+        </div>
+        {!eventStarted ? (
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: '80%'
+            }}
+          >
+            <div style={{ maxWidth: '50%' }}>
+              <H2 style={{ textAlign: 'center' }}>Event Hasn't Started Yet</H2>
+              <H4>Refresh the page once the event has officially begun</H4>
+            </div>
+          </div>
+        ) : (
+            <CasesList />
+          )}
+      </React.Fragment>
+    )}
+    />
+  )
+
 }
 
 export default ChallengesPage
